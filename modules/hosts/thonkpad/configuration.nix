dbeba04586899d30881f1a5a572ded150af05d9f
verminyx <github@lilyt.me>
2026-07-23 09:03:13 +0200

    nixos switch success: 2026-07-23T09:03:13+02:00

--- modules/hosts/thonkpad/configuration.nix
+++ modules/hosts/thonkpad/configuration.nix
@@ -40,8 +40,7 @@
 
     # Enable the X11 windowing system.
     # You can disable this if you're only using the Wayland session.
-  services.xserver.enable = true;
-
+    services.xserver.enable = true;
     # Enable the KDE Plasma Desktop Environment.
     services.displayManager.sddm.enable = true;
     #services.desktopManager.plasma6.enable = true;
@@ -67,12 +66,14 @@
     pulse.enable = true;
     # If you want to use JACK applications, uncomment this
     #jack.enable = true;
-
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
     #media-session.enable = true;
   };
-
+  services.fprintd.enable = true;
+  services.fprintd.tod.enable = true;
+  services.fprintd.tod.driver = pkgs.libfprint-2-tod1-goodix-550a;
+    
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
@@ -86,7 +87,7 @@
     #  thunderbird
     ];
   };
-
+  
   # Install firefox.
   #programs.firefox.enable = true;
 
@@ -99,12 +100,12 @@
     neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     git
-    fprintd
     firefox
     github-cli
     signal-desktop
+    jetbrains.pycharm-oss
+    jetbrains.pycharm
   ];
-
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
